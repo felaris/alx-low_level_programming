7063b79c120e5332ac42a86b3a391f6ef95b5a66
felaris <felixawortwe14@gmail.com>
2022-12-10 21:07:07 -0800

    added the 5th file

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+/**
+*main - Entry point
+*my hello print task
+*Return: Always 0
+*/
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,/n");
-  return (0);
-    
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
-
-
