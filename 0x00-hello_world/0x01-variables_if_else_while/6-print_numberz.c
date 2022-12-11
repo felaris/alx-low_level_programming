bb2c4b5c13af073d261f650e5bda79f2fa1dbb51
felaris <felixawortwe14@gmail.com>
2022-12-10 22:08:31 -0800

    added the 6th file

---
+++ 0x00-hello_world/0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Displays numbers between 0 to 9.
+*
+*Return:0 always (Success)
+*/
+int main(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
