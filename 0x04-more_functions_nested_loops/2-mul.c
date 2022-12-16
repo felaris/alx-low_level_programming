bc8772842b2505fd71886e7b09b4161e00f45353
felaris <felixawortwe14@gmail.com>
2022-12-15 17:35:32 -0800

    added the 2nd file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - check the for multiplication.
+ * @a: character a
+ * @b: character b
+ * Return: success
+ */
+int mul(int a, int b)
+{
+int multiple = a * b;
+return (multiple);
+}
