da8dedf896fb16525ff10cd8e5f4f153dcb4c76c
felaris <felixawortwe14@gmail.com>
2022-12-15 18:40:54 -0800

    added the 10th file

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+* print_triangle -for drawing triangles
+*
+* @size: return nothing
+* Return: 1 for digit and 0 for else
+*/
+void print_triangle(int size)
+{
+int i;
+int j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (j < size - i - 1)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
+}
