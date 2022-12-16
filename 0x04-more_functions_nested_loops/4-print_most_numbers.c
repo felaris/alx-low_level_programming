9a2773ba2dd648b9e184686671973c628c406ad3
felaris <felixawortwe14@gmail.com>
2022-12-15 18:03:49 -0800

    added the 4th file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+* print_most_numbers -checks for numbers from 0 to 9 except 2 and 4
+*
+* @void: return nothing
+* Return: 1 for digit and 0 for else
+*/
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+if (i != 2 && i != 4)
+{
+_putchar('0' + i);
+}
+}
+_putchar('\n');
+}
