70cc5e82acf85e76451f55cc75b545cfd0d68711
felaris <felixawortwe14@gmail.com>
2022-12-15 18:18:52 -0800

    added the 7th file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_diagonal -Draw a diagonal line
+*
+* @n: return nothing
+* Return: 1 for digit and 0 for else
+*/
+void print_diagonal(int n)
+{
+int i;
+int j;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
+_putchar(' ');
+_putchar('\\');
+_putchar('\n');
+}
+}
+}
