b258af02ca8d43fd95042042d66f49e56c653264
felaris <felixawortwe14@gmail.com>
2022-12-15 18:15:27 -0800

    added the 6th file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* print_line - draw a straight line.
+* @n: integer
+* Return: Always 0.
+*/
+void print_line(int n)
+{
+int i;
+for (i = 0; i < n; ++i)
+_putchar('_');
+_putchar('\n');
+return;
+}
