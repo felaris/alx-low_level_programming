8ea5f19a4daefe8e414319aac90459b9ed7a1e8f
felaris <felixawortwe14@gmail.com>
2022-12-15 17:23:08 -0800

    added my 0th file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isupper - check the code for Uppercase letters
+ * @c: characters
+ * Return: 1 and 0
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
