464f34bf14fdbd57d0b0e273912c2d96e2aa3f34
Felix Awortwe Kwamena <felixawortwe14@gmail.com>
2022-12-20 04:42:44 +0000

    corrected error on the 0th file

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,20 +1,10 @@
 #include "main.h"
-
-
-
 /**
  * reset_to_98 - resets value of pointer to 98
- *
  * @n: pointer to reset to 98
- *
  * Return: void
- *
  */
-
-void reset_to_98(int *n)
-
+void reset_to_98(int *n)0
 {
-
-		*n = 98;
-
+*n = 98;
 }
