9093221acb1aa15763012719014ca88b7dde4317
Felix Awortwe Kwamena <felix.awortwe@datamavens.io>
2022-12-22 16:54:49 +0000

    added the 5th file

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* string_toupper -concatinate string
+*
+*@s: stores the input
+*Return: uppercase string
+*/
+char *string_toupper(char *s)
+{
+int c = 0;
+while (s[c] != '\0')
+{
+if (s[c] >= 'a' && s[c] <= 'z')
+{
+s[c] = s[c] - 32;
+}
+c++;
+}
+return (s);
+}
