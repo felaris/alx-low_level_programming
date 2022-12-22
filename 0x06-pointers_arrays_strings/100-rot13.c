a71d466fd2818b3e0768ca77965fbed7547094ee
Felix Awortwe Kwamena <felix.awortwe@datamavens.io>
2022-12-22 17:06:53 +0000

    added the optional task 1

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#define ROT 13
+/**
+* rot13 -concatinate string
+*
+*@c: stores the input
+*Return: uppercase string
+*/
+char *rot13(char *c)
+{
+int e;
+	while((c=getchar())!=EOF)
+	{
+		if(c >='A' && c <='Z')
+		{
+			if((e = c + ROT) <= 'Z')
+				_putchar(e);
+			else
+			{
+				   e = c - ROT;
+				_putchar(e);
+			}
+		}
+		else if(c >='a' && c <='z')
+		{
+			if((e= c + ROT) <= 'z')
+				_putchar(e);
+			else
+			{
+				e = c - ROT;
+				_putchar(e);
+			}
+		}
+		else
+			_putchar(c);
+	}
+
+return (c);
+}
