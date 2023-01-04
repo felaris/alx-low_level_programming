5c80d97132cdada32c2f2a34109ca63c58a56e20
Felix Awortwe Kwamena <felix.awortwe@datamavens.io>
2023-01-04 02:03:08 +0000

    fixed error in the zeroth file

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,30 +1,17 @@
 #include "main.h"
-
-
-
 /**
- * _memset - fills memory with a constant byte
- *
- *   * @s: memory space to be filled
- *
- *    * @b: byte to fill with
- *
- *     * @n: number of spaces to fill
- *
- *      *
- *
- *       * Return: pointer to memory area s
- *
- */
-
+* _memset -memset
+*
+*@s: pointer
+*@b: value to be saved
+*@n: number of characters
+*Return: dest
+*/
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-	{
-		*(s + i) = b;
-	}
-
-return (s);
+unsigned int index;
+char *memory = s, value = b;
+for (index = 0; index < n; index++)
+memory[index] = value;
+return (memory);
 }
